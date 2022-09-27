03ba3d6e8f3b64769e5cadacd8ca6c2e48635723
Natálie Vychodilová <vychodilova.natalie@gmail.com>
2022-09-27 10:21:07 +0200

    changes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.HashMap;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("HEll");
+    public static void main(String[] args) throws NoSuchFieldException {
+        MapClass map = new MapClass();
+
+        map.store(4,"Rada");
+        map.store(0,"Jirka");
+        map.store(1,"Naty");
+        map.store(2,"Moni");
+        map.store(3,"Kaja");
+        map.store(8,"XXX");
+
+        //map.store(4,"Rada");
+        //map.getValue(5);
+        //map.deleteKey(5);
+
+        map.deleteKey(4);
+
+        map.print();
+        System.out.println(map.getSize());
     }
 }
---
+++ src/MapClass.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+
+public class MapClass {
+    HashMap<Integer, String> hashmap = new HashMap<>();
+
+    public void store(Integer id, String value){
+        if (!hashmap.containsKey(id)){
+            hashmap.put(id, value);
+        } else {
+            throw new ArrayStoreException("Already Exists!");
+        }
+    }
+
+    public String getValue(Integer id) throws NoSuchFieldException {
+        if (hashmap.containsKey(id)){
+            return hashmap.get(id);
+        } else {
+            throw new NoSuchFieldException("Does not Exists!");
+        }
+    }
+
+    public void deleteKey(Integer id) throws NoSuchFieldException {
+        if (hashmap.containsKey(id)){
+            hashmap.remove(id);
+        } else {
+            throw new NoSuchFieldException("Does not Exists!");
+        }
+    }
+
+    public int getSize(){
+        return hashmap.size();
+    }
+
+    public void print(){
+        hashmap.forEach((id, value) -> {
+            System.out.print(id + "->" + value + "\n");
+        });
+    }
+}
